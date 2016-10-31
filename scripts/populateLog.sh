b8d3213ce9c8c36019f4e1ed6eccb45ced231096
tomecian <christian.tomeldan@gmail.com>
2016-10-30 21:54:54 -0700

    Updating populateLog script to change the command parameters used

--- scripts/populateLog.sh
+++ scripts/populateLog.sh
@@ -5,5 +5,5 @@ password=$2
 host=$3
 
 for i in `seq 1 5000`; do
-        $(wget -O /dev/null -o /dev/null -nv "http://$1:$2@$3")
+        $(wget -O /dev/null --user $1 --password $2 "http://$3")
 done
